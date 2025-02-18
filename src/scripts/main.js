4c4593781ba2a890234bb0f342ddffaa122ae3a5
EgorMamtsev <egor.mamtsev.dru@gmail.com>
2025-02-18 12:48:07 -0600

    almost finished, make sort if not a number

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const head = document.querySelectorAll('thead th');
+
+function sortTablet(i) {
+  const tbody = document.querySelector('tbody');
+  const body = [...document.querySelectorAll('tbody tr')];
+
+  if (isNaN(body[i].children.textContent)) {
+    // дороби перевірку коли значення нє є числом,
+    // прибери зайві символи та порівняй з localeCompare
+  }
+
+  body.sort((a, b) => a.children[i].textContent - b.children[i].textContent);
+
+  body.forEach((el) => tbody.append(el));
+}
+
+head.forEach((el, index) => {
+  el.addEventListener('click', () => {
+    sortTablet(index);
+  });
+});
+// readme відредаговано і закомічено
